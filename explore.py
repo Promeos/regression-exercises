7a4bd651149be9a9467652973b3c7d7c73bd2927
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-09-28 02:02:33 -0500

    upload files

---
+++ explore.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
+def plot_variable_pairs(df):
+    pass
+
+def months_to_years(tenure_months, df):
+    pass
+    # return years
+    
+def plot_categorical_and_continuous_vars(categorical_var, continuous_var, df):
+    pass
+    # return 3 different plots for a categorical variable with a continuous variable
+
+
+
+
+
+#    j = sns.jointplot("exam1", "final_grade", data=train, kind='reg', height=5) 
