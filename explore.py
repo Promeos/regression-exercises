f45938b20c88a5c98734707e86c13a86e3f6be52
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-09-28 21:45:30 -0500

    updated files

--- explore.py
+++ explore.py
@@ -6,18 +6,34 @@ import matplotlib.pyplot as plt
 
 
 def plot_variable_pairs(df):
-    pass
+    '''
+    Accepts the telco_churn train set
+    Returns all pairwise relationships between attributes
+    '''
+    
+    columns_to_plot = df.select_dtypes(exclude='O').columns.values
 
-def months_to_years(tenure_months, df):
-    pass
-    # return years
+    for column in columns_to_plot:
+        for pair in columns_to_plot:
+            if column != pair:
+                sns.regplot(x=column,
+                            y=pair,
+                            data=df,
+                            line_kws={"color": "red"},
+                           ).set_title(column + " and " + pair)
+                plt.show()
+
+
+def months_to_years(df):
+    df['tenure_years'] = round(df.tenure / 12, 0)
+    df = df[['customer_id',
+             'monthly_charges',
+             'tenure',
+             'tenure_years',
+             'total_charges'
+    ]]
+    return df
     
 def plot_categorical_and_continuous_vars(categorical_var, continuous_var, df):
     pass
     # return 3 different plots for a categorical variable with a continuous variable
-
-
-
-
-
-#    j = sns.jointplot("exam1", "final_grade", data=train, kind='reg', height=5) 
