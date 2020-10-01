85db74cbed30c11639ced1774d05cf82a7857164
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-10-01 17:07:46 -0500

    upload files

--- evaluate.py
+++ evaluate.py
@@ -1,1 +1,11 @@
-pass
+import matplotlib.pyplot as plt
+
+
+def plot_residuals(actual, predicted):
+    residuals = actual - predicted
+    plt.hlines(0, actual.min(), actual.max(), ls=':')
+    plt.scatter(actual, residuals)
+    plt.ylabel('residual ($y - \hat{y}$)')
+    plt.xlabel('actual value ($y$)')
+    plt.title('Actual vs Residual')
+    return plt.gca()
